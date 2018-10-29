f17dd15004be635205c73fbd9d3cb8f471483296
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-10-29 17:33:52 -0500

    (rxjs): Lab 1 - Ticket Search DropDown

--- libs/ticket-list-view/src/lib/search-tickets/search-tickets.component.ts
+++ libs/ticket-list-view/src/lib/search-tickets/search-tickets.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { TicketService, UserService } from '@tuskdesk-suite/backend';
 import { User } from '@tuskdesk-suite/data-models';
+import { Subscription } from 'rxjs';
 
 interface SearchResult {
   id: number;
@@ -15,16 +16,29 @@ interface SearchResult {
   templateUrl: './search-tickets.component.html',
   styleUrls: ['./search-tickets.component.scss']
 })
-export class SearchTicketsComponent implements OnInit {
+export class SearchTicketsComponent implements OnInit, OnDestroy {
+  usersFound: User[];
   searchTerm = new FormControl();
   assignedToUser = new FormControl();
 
-  usersFound: User[];
   searchResults$: Observable<SearchResult[]>;
+  subscription: Subscription;
 
   constructor(private ticketService: TicketService, private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.assignedToUser.valueChanges.subscribe(searchTerm => {
+      this.userService.users(searchTerm).subscribe(users => {
+        this.usersFound = users;
+      });
+    });
+  }
 
-  submit() {}
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  submit() {
+    this.searchResults$ = this.ticketService.searchTickets(this.searchTerm.value, this.assignedToUser.value);
+  }
 }
