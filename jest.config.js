de8bbe359a451138e06a451aa7d86d2cb3990773
Zack DeRose <zack@nrwl.io>
2019-07-02 21:17:42 -0700

    chore: Upgrade to angular 8 dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-	transform: {
-    '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
-	},
-  resolver: '@nrwl/builders/plugins/jest/resolver',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest'
+  },
+  resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
+  collectCoverage: true,
   coverageReporters: ['html']
 };
--- libs/ui-material/src/lib/ui-material.module.ts
+++ libs/ui-material/src/lib/ui-material.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatInputModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatToolbarModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { BidiModule } from '@angular/cdk/bidi';
 
