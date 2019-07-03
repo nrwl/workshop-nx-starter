11ce329030f67f05cd5364aa9526d46406017bc4
Zack DeRose <zack@nrwl.io>
2019-07-03 14:40:56 -0700

    chore(api): create e2e app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,15 +7,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+export async function bootstrap(port?: number) {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
+  if (!port) {
+    port = +process.env.port || 3333;
+  }
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 
 bootstrap();
-    
