17f650f6adc81782a7cabaa51cdd828933f2b5ac
Zack DeRose <zack@nrwl.io>
2019-07-03 17:41:02 -0700

    fix(api): event-logs now tracks event

--- libs/api/event-logs/feature/src/lib/event-logs.controller.ts
+++ libs/api/event-logs/feature/src/lib/event-logs.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { EventLogService } from '@tuskdesk-suite/api/event-logs/data-access/src';
+import { Request } from 'express';
 
 @Controller('event-logs')
 export class EventLogsController {
   constructor(private eventLogService: EventLogService) {}
 
   @Get()
-  getAllEventLogs() {
-    return this.eventLogService.findAll();
+  getAllEventLogs(@Req() request: Request) {
+    const eventLogs = this.eventLogService.findAll();
+    this.eventLogService.trackEvent(
+      request,
+      'event-log',
+      'viewed all EVENTLOGS'
+    );
+    return eventLogs;
   }
 }
