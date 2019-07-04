bab663db33d6f524dace5024e9ef48c80936c8ce
Zack DeRose <zack@nrwl.io>
2019-07-03 17:05:27 -0700

    chore(api): tickets feature refactored

--- libs/api/tickets/data-access/src/lib/api-tickets-data-access.module.ts
+++ libs/api/tickets/data-access/src/lib/api-tickets-data-access.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { ApiUsersDataAccessModule } from '@tuskdesk-suite/api/users/data-access';
+import { ApiEventLogsDataAccessModule } from '@tuskdesk-suite/api/event-logs/data-access';
+import { ApiCommentsDataAccessModule } from '@tuskdesk-suite/api/comments/data-access';
 
 @Module({
-  imports: [ApiUsersDataAccessModule],
+  imports: [
+    ApiUsersDataAccessModule,
+    ApiEventLogsDataAccessModule,
+    ApiCommentsDataAccessModule
+  ],
   controllers: [],
   providers: [TicketService],
   exports: [TicketService]
