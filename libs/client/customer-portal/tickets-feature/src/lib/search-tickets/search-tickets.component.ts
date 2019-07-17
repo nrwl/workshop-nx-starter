6d25c7562f944f6a551440726432fb580e51cdb8
Zack DeRose <zack@nrwl.io>
2019-07-17 08:05:23 -0700

    feat: lab-2-1 complete

--- libs/client/customer-portal/tickets-feature/src/lib/search-tickets/search-tickets.component.ts
+++ libs/client/customer-portal/tickets-feature/src/lib/search-tickets/search-tickets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import {
@@ -6,6 +6,7 @@ import {
   UserService
 } from '@tuskdesk-suite/client/shared/tuskdesk-api-data-access';
 import { User } from '@tuskdesk-suite/shared/user-utils';
+import { Subscription } from 'rxjs';
 
 interface SearchResult {
   id: number;
@@ -14,23 +15,41 @@ interface SearchResult {
 }
 
 @Component({
-  selector: 'app-search-tickets',
+  selector: 'tuskdesk-suite-search-tickets',
   templateUrl: './search-tickets.component.html',
   styleUrls: ['./search-tickets.component.scss']
 })
-export class SearchTicketsComponent implements OnInit {
+export class SearchTicketsComponent implements OnInit, OnDestroy {
   searchTerm = new FormControl();
   assignedToUser = new FormControl();
 
   usersFound: User[];
   searchResults$: Observable<SearchResult[]>;
+  subscription: Subscription;
 
   constructor(
     private ticketService: TicketService,
     private userService: UserService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.assignedToUser.valueChanges.subscribe(
+      searchTerm => {
+        this.userService.users(searchTerm).subscribe(users => {
+          this.usersFound = users;
+        });
+      }
+    );
+  }
 
-  submit() {}
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  submit() {
+    this.searchResults$ = this.ticketService.searchTickets(
+      this.searchTerm.value,
+      this.assignedToUser.value
+    );
+  }
 }
