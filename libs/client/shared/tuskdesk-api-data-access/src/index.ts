48041eede256dc8e7f9e845ea9f886fd7b0ac653
Zack DeRose <zack@nrwl.io>
2019-07-17 07:44:29 -0700

    feat: lab-1-3 complete

--- libs/client/shared/tuskdesk-api-data-access/src/index.ts
+++ libs/client/shared/tuskdesk-api-data-access/src/index.ts
@@ -4,3 +4,4 @@ export { TicketService } from './lib/ticket.service';
 export { UserService } from './lib/user.service';
 export { LoggedInUserInterceptor } from './lib/logged-in-user-interceptor';
 export { BackendUserIdService } from './lib/backend-user-id.service';
+export { ApiConfig } from './lib/api-config';
