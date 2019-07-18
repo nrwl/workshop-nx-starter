f41b64a76a95f5e64a9e5f6efaa23a380ec57369
Zack DeRose <zack@nrwl.io>
2019-07-18 07:46:20 -0700

    feat: complete lab 3-2

--- libs/client/customer-portal/tickets-data-access/src/lib/+state/tickets.selectors.ts
+++ libs/client/customer-portal/tickets-data-access/src/lib/+state/tickets.selectors.ts
@@ -1,23 +1,35 @@
 import { Ticket } from '@tuskdesk-suite/shared/ticket-utils';
-import { PartialAppState } from './tickets.interfaces';
+import { PartialAppState, TicketsState } from './tickets.interfaces';
 import { FEATURE_TICKETS } from './tickets.reducer';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export namespace ticketsQuery {
-  export const getIsLoading = (state: PartialAppState) =>
-    state[FEATURE_TICKETS].loading;
-  export const getError = (state: PartialAppState) =>
-    state[FEATURE_TICKETS].error;
-  export const getSelectedId = (state: PartialAppState) =>
-    state[FEATURE_TICKETS].selectedId;
-  export const getAllTickets = (state: PartialAppState) =>
-    state[FEATURE_TICKETS].list;
+  const selectTicketState = createFeatureSelector<TicketsState>(
+    FEATURE_TICKETS
+  );
+  export const getIsLoading = createSelector(
+    selectTicketState,
+    state => state.loading
+  );
+  export const getError = createSelector(
+    selectTicketState,
+    state => state.error
+  );
+  export const getSelectedId = createSelector(
+    selectTicketState,
+    state => state.selectedId
+  );
+  export const getAllTickets = createSelector(
+    selectTicketState,
+    state => state.list
+  );
 
-  export const getSelectedTicket = (state: PartialAppState) => {
-    const selectedId = getSelectedId(state);
-    const tickets = getAllTickets(state);
-    const matchingIds = (it: Ticket) => it.id === selectedId;
-    const ticket = selectedId ? tickets.find(matchingIds) : null;
-
-    return ticket ? ({ ...ticket } as Ticket) : null;
-  };
+  export const getSelectedTicket = createSelector(
+    getAllTickets,
+    getSelectedId,
+    (tickets, id) => {
+      const ticket = id ? tickets.find(x => x.id === id) : null;
+      return ticket ? ({ ...ticket } as Ticket) : null;
+    }
+  );
 }
