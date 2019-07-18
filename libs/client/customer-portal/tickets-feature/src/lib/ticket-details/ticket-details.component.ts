9d52fc9106356f6f1f76527f8f9925d46e267ea8
Zack DeRose <zack@nrwl.io>
2019-07-18 07:04:57 -0700

    feat: finished lesson 3-1

--- libs/client/customer-portal/tickets-feature/src/lib/ticket-details/ticket-details.component.ts
+++ libs/client/customer-portal/tickets-feature/src/lib/ticket-details/ticket-details.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Ticket } from '@tuskdesk-suite/shared/ticket-utils';
@@ -6,6 +11,22 @@ import { Comment } from '@tuskdesk-suite/shared/comment-utils';
 import { FormControl } from '@angular/forms';
 import { TicketService } from '@tuskdesk-suite/client/shared/tuskdesk-api-data-access';
 import { TicketTimerService } from '../ticket-timer.service';
+import { Store, select } from '@ngrx/store';
+import {
+  ticketsQuery,
+  ClientCustomerPortalTicketsDataAccessModule,
+  ticketLoaded
+} from '@tuskdesk-suite/client/customer-portal/tickets-data-access';
+import {
+  map,
+  publishReplay,
+  refCount,
+  switchMap,
+  tap,
+  takeUntil,
+  take
+} from 'rxjs/operators';
+import { combineLatest, Subscription, Subject } from 'rxjs';
 
 @Component({
   selector: 'tuskdesk-suite-ticket-details',
@@ -17,18 +38,31 @@ export class TicketDetailsComponent implements OnInit {
   comments$: Observable<Comment[]>;
   ticketMessage = new FormControl();
   timer$: Observable<number>;
+  private id$ = this.route.params.pipe(map(params => +params['id']));
+  onDestroy$ = new Subject<void>();
 
   constructor(
+    private store: Store<any>,
     private service: TicketService,
     private route: ActivatedRoute,
     private ticketTimerService: TicketTimerService
   ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      const id = +params['id'];
-      this.ticket$ = this.service.ticketById(id);
-    });
+    // get a ticket to render to the UI
+    this.ticket$ = combineLatest([
+      this.store.pipe(select(ticketsQuery.getAllTickets)),
+      this.id$
+    ]).pipe(map(([tickets, id]) => tickets.find(ticket => ticket.id === id)));
+
+    // retrieve the ticket from the api and stash it in the store
+    this.id$
+      .pipe(
+        take(1),
+        switchMap(id => this.service.ticketById(id)),
+        tap(ticket => this.store.dispatch(ticketLoaded({ ticket })))
+      )
+      .subscribe();
   }
 
   switchToEdit() {}
